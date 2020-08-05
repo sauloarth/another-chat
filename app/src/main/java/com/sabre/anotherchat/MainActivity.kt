8922d703bdb9e82622dc225a62ed5a8c358be92d
Saulo Reis <sauloarth@gmail.com>
2020-08-05 17:43:26 -0300

    Creating basic fragments and setting into ViewPager

--- app/src/main/java/com/sabre/anotherchat/MainActivity.kt
+++ app/src/main/java/com/sabre/anotherchat/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.sabre.anotherchat
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
+import com.sabre.anotherchat.fragments.ChatFragment
+import com.sabre.anotherchat.fragments.SearchFragment
+import com.sabre.anotherchat.fragments.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +27,14 @@ class MainActivity : AppCompatActivity() {
 
         val tabLayout: TabLayout = findViewById(R.id.tab_layout_main)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
+
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+        viewPagerAdapter.addFragment(ChatFragment(), "Chats")
+        viewPagerAdapter.addFragment(SearchFragment(), "Search")
+        viewPagerAdapter.addFragment(SettingsFragment(), "Settings")
+
+        viewPager.adapter = viewPagerAdapter
+        tabLayout.setupWithViewPager(viewPager)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -39,5 +51,30 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private inner class ViewPagerAdapter(fm: FragmentManager):
+            FragmentPagerAdapter(fm) {
+
+        private val fragments: ArrayList<Fragment>
+        private val titles: ArrayList<String>
+
+        init {
+            fragments = ArrayList<Fragment>()
+            titles = ArrayList<String>()
+        }
+
+        override fun getItem(position: Int): Fragment = fragments[position]
+
+        override fun getCount(): Int = fragments.size
+
+        fun addFragment(fragment: Fragment, title: String) {
+            fragments.add(fragment)
+            titles.add(title)
+        }
+
+        override fun getPageTitle(i: Int): CharSequence? = titles[i]
+
+
     }
 }
